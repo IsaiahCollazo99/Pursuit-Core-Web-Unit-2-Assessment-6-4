7af8325056352514c629fd3025de9f64674c2bab
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2019-12-18 19:20:32 -0500

    add fetchData() and populateSelect()

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
-    
+    let select = document.querySelector("#ghibliMovies");
+    let form = document.querySelector("form");
+    let allReviews = document.querySelector("#allReviews");
+    let userReview = document.querySelector("#review");
+    let currentFilm = document.querySelector("#film");
+
+    const fetchData = async (url, callback) => {
+        try {
+            let res = await axios.get(url);
+            callback(res.data);
+        } catch(err) {
+            console.log(err);
+        }
+        
+    }
+
+    const populateSelect = async (data) => {
+        data.forEach(film => {
+            let option = document.createElement("option");
+            option.innerText = film.title;
+            option.id = film.id;
+            select.appendChild(option);
+        })
+    }
+
+    fetchData("https://ghibliapi.herokuapp.com/films", populateSelect);
 })
